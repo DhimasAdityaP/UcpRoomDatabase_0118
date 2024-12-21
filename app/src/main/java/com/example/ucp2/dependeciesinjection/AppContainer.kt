312a6a6b9dcbc71b6bdec7e4564bfd2f989023e4
DhimasAdityaP <dimasadit0105@gmail.com>
2024-12-21 15:21:26 +0700

    inisialisasi repo dengan dao yg sesuai

--- app/src/main/java/com/example/ucp2/dependeciesinjection/AppContainer.kt
+++ app/src/main/java/com/example/ucp2/dependeciesinjection/AppContainer.kt
@@ -1,7 +1,10 @@
 package com.ucp2.dependenciesinjection
 
 import android.content.Context
+import com.example.ucp2.repository.LocalRepositoryDosen
+import com.example.ucp2.repository.LocalRepositoryMatakuliah
 import com.example.ucp2.repository.RepositoryMatakuliah
+import com.pam.pam_ucp2.data.database.KrsDatabase
 import com.ucp2.repository.RepositoryDosen
 
 
@@ -12,6 +15,14 @@ interface InterfaceContainerApp {
 }
 // Implementasi InterfaceContainerApp
 class AppContainer(private val context: Context) : InterfaceContainerApp {
+    // Inisialisasi RepositoryDosen menggunakan DAO yang sesuai
+    override val repositoryDosen: RepositoryDosen by lazy {
+        LocalRepositoryDosen(KrsDatabase.getDatabase(context).dosenDao())
+    }
 
+    // Inisialisasi RepositoryMatakuliah menggunakan DAO yang sesuai
+    override val repositoryMatakuliah: RepositoryMatakuliah by lazy {
+        LocalRepositoryMatakuliah(KrsDatabase.getDatabase(context).matakuliahDao())
+    }
 }
 
