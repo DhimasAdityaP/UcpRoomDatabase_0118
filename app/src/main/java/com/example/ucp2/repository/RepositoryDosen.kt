e7a9a04709481485287a765a5278d6985fe3fd24
DhimasAdityaP <dimasadit0105@gmail.com>
2024-12-21 15:06:50 +0700

    add repo fun dosen

--- app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
@@ -1,1 +1,13 @@
 package com.ucp2.repository
+
+
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryDosen {
+    suspend fun insertDosen(dosen: Dosen)
+    //getAllDosen
+    fun getAllDosen(): Flow<List<Dosen>>
+    //getDosen
+    fun getDosen(nidn: String): Flow<Dosen>
+}
