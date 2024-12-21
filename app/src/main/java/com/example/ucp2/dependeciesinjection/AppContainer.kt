ffca74cd39024826d95bdc02fd393d75ae5d59ef
DhimasAdityaP <dimasadit0105@gmail.com>
2024-12-21 15:18:05 +0700

    implement InterfaceContainerApp

--- app/src/main/java/com/example/ucp2/dependeciesinjection/AppContainer.kt
+++ app/src/main/java/com/example/ucp2/dependeciesinjection/AppContainer.kt
@@ -1,5 +1,6 @@
 package com.ucp2.dependenciesinjection
 
+import android.content.Context
 import com.example.ucp2.repository.RepositoryMatakuliah
 import com.ucp2.repository.RepositoryDosen
 
@@ -9,3 +10,8 @@ interface InterfaceContainerApp {
     val repositoryDosen: RepositoryDosen
     val repositoryMatakuliah: RepositoryMatakuliah
 }
+// Implementasi InterfaceContainerApp
+class AppContainer(private val context: Context) : InterfaceContainerApp {
+
+}
+
