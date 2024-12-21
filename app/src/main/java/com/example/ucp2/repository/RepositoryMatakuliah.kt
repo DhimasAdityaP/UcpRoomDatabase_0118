a8b280b8a76278b062c789e749dd74085f41f87f
DhimasAdityaP <dimasadit0105@gmail.com>
2024-12-21 15:08:32 +0700

    add repo fun mata kuliah

--- app/src/main/java/com/example/ucp2/repository/RepositoryMatakuliah.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryMatakuliah.kt
@@ -1,1 +1,13 @@
 package com.example.ucp2.repository
+
+import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryMatakuliah {
+    fun getAllMatakuliah(): Flow<List<MataKuliah>>
+    suspend fun insertMatakuliah(matakuliah: MataKuliah)
+    fun getMatakuliah(kode: String): Flow<MataKuliah>
+    suspend fun deleteMatakuliah(matakuliah: MataKuliah)
+    suspend fun updateMatakuliah(matakuliah: MataKuliah)
+
+}
