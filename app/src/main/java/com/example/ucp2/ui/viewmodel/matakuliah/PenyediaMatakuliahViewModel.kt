dc5e525efcbb67f0e8df7c449ce410fd45f07f4a
DhimasAdityaP <dimasadit0105@gmail.com>
2024-12-21 16:31:40 +0700

    add factory di PenyediaMatakuliahViewModel

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/PenyediaMatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/PenyediaMatakuliahViewModel.kt
@@ -1,1 +1,42 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.KrsApp
+import com.ucp2.ui.viewmodel.matakuliah.DetailMatakuliahViewModel
+
+object PenyediaMatakuliahViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            MatakuliahViewModel(
+                KrsApp().AppContainer.repositoryMatakuliah
+            )
+        }
+        initializer {
+            HomeMatakuliahViewModel(
+                KrsApp().AppContainer.repositoryMatakuliah,
+            )
+        }
+        initializer {
+            DetailMatakuliahViewModel(
+                createSavedStateHandle(),
+                KrsApp().AppContainer.repositoryMatakuliah,
+            )
+        }
+        initializer {
+            UpdateMatakuliahViewModel(
+                createSavedStateHandle(),
+                KrsApp().AppContainer.repositoryMatakuliah,
+            )
+        }
+    }
+}
+
+fun CreationExtras.KrsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
+
+
