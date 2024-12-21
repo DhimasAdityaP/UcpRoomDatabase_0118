0108598f3d8b501f10533befcb9520f9d0898619
DhimasAdityaP <dimasadit0105@gmail.com>
2024-12-21 15:46:05 +0700

    add DetailDosenViewModel dan fun dosenvent

--- app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DetailDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DetailDosenViewModel.kt
@@ -1,1 +1,80 @@
-package com.pam.pam_ucp2.ui.viewmodel.dosen
+package com.ucp2.ui.viewmodel.dosen
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dosen
+import com.ucp2.repository.RepositoryDosen
+import com.ucp2.ui.navigation.DestinasiDosenDetail
+import com.ucp2.ui.viewmodel.dosen.DosenEvent
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class DetailDosenViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDosen: RepositoryDosen,
+) : ViewModel() {
+
+    // Mendapatkan NIDN dari SavedStateHandle
+    private val _nidn: String = checkNotNull(savedStateHandle[DestinasiDosenDetail.NIDN])
+
+    // Mendapatkan detail mahasiswa dan mengelola UI state
+    val detailUiState: StateFlow<DetailUiState> = repositoryDosen.getDosen(_nidn)
+        .filterNotNull() // Pastikan data tidak null
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true)) // Set status loading saat mulai
+            delay(600) // delay
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+}
+
+// Data class untuk UI State
+data class DetailUiState(
+    val detailUiEvent: DosenEvent = DosenEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == DosenEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != DosenEvent()
+}
+
+// Fungsi ekstensi untuk mengubah dosen menjadi dosen event
+fun Dosen.toDetailUiEvent(): DosenEvent {
+    return DosenEvent(
+        nidn = nidn,
+        nama = nama,
+        jenisKelamin = jeniskelamin
+    )
+}
+
