2eaf6a5e7096ffbc9f4aaed5fdb2a0653b301ca7
DhimasAdityaP <dimasadit0105@gmail.com>
2024-12-21 15:12:31 +0700

    add local repo fun Matakuliah

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryMatakuliah.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryMatakuliah.kt
@@ -1,1 +1,24 @@
 package com.example.ucp2.repository
+import com.example.ucp2.data.dao.MatakuliahDao
+import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryMatakuliah (
+    private val matakuliahDao: MatakuliahDao
+) : RepositoryMatakuliah {
+    override fun getAllMatakuliah(): Flow<List<MataKuliah>> {
+        return matakuliahDao.getAllMatakuliah()
+    }
+    override fun getMatakuliah(kode: String): Flow<MataKuliah> {
+        return matakuliahDao.getMatakuliah(kode)
+    }
+    override suspend fun insertMatakuliah(matakuliah: MataKuliah) {
+        matakuliahDao.insertMatakuliah(matakuliah)
+    }
+    override suspend fun deleteMatakuliah(matakuliah: MataKuliah) {
+        matakuliahDao.deleteMatakuliah(matakuliah)
+    }
+    override suspend fun updateMatakuliah(matakuliah: MataKuliah) {
+        matakuliahDao.updateMatakuliah(matakuliah)
+    }
+}
