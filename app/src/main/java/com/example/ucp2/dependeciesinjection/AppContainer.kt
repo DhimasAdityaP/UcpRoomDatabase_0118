3bbc2e2a6a418ee74cdd835178d6cc47b04b623e
DhimasAdityaP <dimasadit0105@gmail.com>
2024-12-21 15:16:07 +0700

    add InterfaceContainerApp

--- app/src/main/java/com/example/ucp2/dependeciesinjection/AppContainer.kt
+++ app/src/main/java/com/example/ucp2/dependeciesinjection/AppContainer.kt
@@ -1,2 +1,11 @@
 package com.ucp2.dependenciesinjection
 
+import com.example.ucp2.repository.RepositoryMatakuliah
+import com.ucp2.repository.RepositoryDosen
+
+
+// Interface container app
+interface InterfaceContainerApp {
+    val repositoryDosen: RepositoryDosen
+    val repositoryMatakuliah: RepositoryMatakuliah
+}
